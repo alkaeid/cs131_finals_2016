88ce2292c6825d75dff53ae66b1a47993951caee
Luis Angelo Gutierrez <luan_gutierrez@yahoo.com>
2017-03-16 15:21:57 +0800

    Add files via upload

--- CS131 Prob4/ODE1.java
+++ CS131 Prob4/ODE1.java
@@ -1,8 +1,6 @@
-/*
--read driverRK4 for instructions
--returns the value of the function after substitution
-*/
-
+/** Contains the formula for the first ordinary differential equation
+  */
+package com.prob4.product;
 public class ODE1{
 	public static double f(double t, double x, double y){
 		return ((0.00025*x*y)-(.7*x));
--- CS131 Prob4/ODE2.java
+++ CS131 Prob4/ODE2.java
@@ -1,8 +1,6 @@
-/*
--read driverRK4 for instructions
--returns the value of the function after substitution
-*/
-
+/** Contains the formula for the second ordinary differential equation
+  */
+package com.prob4.product;
 public class ODE2{
 	public static double f(double t, double x, double y){
 		return ((1.1*y)-(0.0005*x*y));
